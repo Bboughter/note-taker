3b215951c15b32b30ffd320f71abc02385e38320
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-04-11 19:47:13 -0700

    added middleware and established the port

--- server.js
+++ server.js
@@ -2,3 +2,14 @@ const express = require('express');
 const path = require("path");
 const fs = require("fs");
 const util = require("util");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use(express.static('public'));
