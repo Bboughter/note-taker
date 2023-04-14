d062ee00f45dda05c53849666d926d1da6719869
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-04-13 20:30:36 -0700

    added comments to server.js file

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const express = require('express');
 const path = require("path");
 const fs = require("fs");
 
+//PORT to run the server on
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//middleware
 app.use(express.json());
 app.use(express.static('public'));
 
 let notes = [];
 
+//read from file db.json
 fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if(err) {
         console.error(err);
@@ -18,10 +21,12 @@ fs.readFile('./db/db.json', 'utf8', (err, data) => {
     notes = JSON.parse(data);
 });
 
+//get route for api/notes
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
+//post route for api/notes to add new note
 app.post('/api/notes', (req, res) => {
     const newNote = {
         id: notes.length + 1,
@@ -39,10 +44,12 @@ app.post('/api/notes', (req, res) => {
     });
 });
 
+//get route for any path that matches with public/notes.html
 app.get('*',(req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
+//delete route for api/notes/:id using the id as a parameter to delete the note
 app.delete('/api/notes/:id', (req, res) => {
     const noteId = parseInt(req.params.id);
     const index = notes.findIndex(note => note.id === noteId);
@@ -61,6 +68,7 @@ app.delete('/api/notes/:id', (req, res) => {
     }
 });
 
+//PORT listener 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
